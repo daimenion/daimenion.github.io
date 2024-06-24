6905fc5f8bdf7e47292a0fc84ae1c05f5fff1f3a
daimenion <daimenion@hotmail.com>
2024-06-23 22:10:52 -0400

    With UI work updates

---
+++ DefaultScript.js
@@ -0,0 +1,17 @@
+(function($) {
+
+	var $window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$titleBar = null,
+		$nav = $('#nav'),
+		$wrapper = $('#wrapper');
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				
+			}, 100);
+		});
+
+})(jQuery);
